1f867f1ad7266416c8539100de9f1583591c2efa
dirre44 <dirre44@gmail.com>
2023-11-07 14:33:20 -0300

    funciona con pywhatkit

--- enviarWPP.py
+++ enviarWPP.py
@@ -8,10 +8,29 @@ from leer_mails import leer_mails
 
 lista_mails = leer_mails()
 
+codigo_wpp_prueba="ECWo66rKJkN8pEAvoqxQwN"
+codigo_wpp_FOB="HScd4MqiFSJC1ujQymJFoW"
+
 #for mail in lista_mails:
 #    mail.replace(r'\r\n  VERIFICAR HABER PEDIDO ANTES PARA NO SUPERPONER EN FOB\r\n', '')
-#    print(mail)
+    #mail_limpio=mail.strip()
+    #mail_limpio=mail_limpio.replace('VERIFICAR HABER PEDIDO ANTES PARA NO SUPERPONER EN FOB', '')
+    #mail_final=f"comprar en FOB:\n{mail_limpio} (STOCK)"
+    #print(f"comprar en FOB:\n{mail_limpio}(STOCK)")
 
 for mail in lista_mails:
 
-    pywhatkit.sendwhatmsg_to_group_instantly("ECWo66rKJkN8pEAvoqxQwN",mail,15,True,3)
+    mail_limpio=mail.strip()
+    mail_limpio=mail_limpio.replace('VERIFICAR HABER PEDIDO ANTES PARA NO SUPERPONER EN FOB', '')
+    #mail_final=f"comprar en FOB:\n{mail_limpio} (STOCK)"
+    mail_final=f"comprar en FOB:\n{mail_limpio}(STOCK)"
+    print(mail_final)
+
+    seleccion = input("Desea enviar este mensaje de falta de stock?? Y/N: " )
+    if seleccion.upper() == "Y":
+        pywhatkit.sendwhatmsg_to_group_instantly(codigo_wpp_FOB,mail_final,8,True,3)
+    elif seleccion.upper() == "N":
+        continue
+    else:
+        # en realidad no se que deberia hacer en esta situacion, un continue??
+        break
