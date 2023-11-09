152852c4d5f479d7e238545b1950c17ffcc782f1
dirre44 <dirre44@gmail.com>
2023-11-09 11:36:10 -0300

    Enviar mensajes a FOB automaticamente

--- enviarWPP.py
+++ enviarWPP.py
@@ -1,36 +1,37 @@
 import pywhatkit
 from leer_mails import leer_mails
 
-# id del grupo ECWo66rKJkN8pEAvoqxQwN
-
-#pywhatkit.sendwhatmsg_instantly("+5491154193721", "chino gil", 7, False)
-
-
-lista_mails = leer_mails()
+def limpiar_mail(lista_mail):
+    mails_limpios=[]
+    for mail in lista_mail:
+        mail_limpio=mail.strip()
+        mail_limpio=mail_limpio.replace('VERIFICAR HABER PEDIDO ANTES PARA NO SUPERPONER EN FOB', '')
+        mail_final=f"comprar en FOB:\n{mail_limpio}(STOCK)"
+        mails_limpios.append(mail_final)    
+    return mails_limpios
 
 codigo_wpp_prueba="ECWo66rKJkN8pEAvoqxQwN"
 codigo_wpp_FOB="HScd4MqiFSJC1ujQymJFoW"
 
-#for mail in lista_mails:
-#    mail.replace(r'\r\n  VERIFICAR HABER PEDIDO ANTES PARA NO SUPERPONER EN FOB\r\n', '')
-    #mail_limpio=mail.strip()
-    #mail_limpio=mail_limpio.replace('VERIFICAR HABER PEDIDO ANTES PARA NO SUPERPONER EN FOB', '')
-    #mail_final=f"comprar en FOB:\n{mail_limpio} (STOCK)"
-    #print(f"comprar en FOB:\n{mail_limpio}(STOCK)")
+lista_mails = leer_mails()
+lista_mensajes=limpiar_mail(lista_mails)
 
-for mail in lista_mails:
+division_mensajes={"enviar":[],"no_enviar":[]}
 
-    mail_limpio=mail.strip()
-    mail_limpio=mail_limpio.replace('VERIFICAR HABER PEDIDO ANTES PARA NO SUPERPONER EN FOB', '')
-    #mail_final=f"comprar en FOB:\n{mail_limpio} (STOCK)"
-    mail_final=f"comprar en FOB:\n{mail_limpio}(STOCK)"
-    print(mail_final)
+for mail in lista_mensajes:
 
+    print(mail)
     seleccion = input("Desea enviar este mensaje de falta de stock?? Y/N: " )
     if seleccion.upper() == "Y":
-        pywhatkit.sendwhatmsg_to_group_instantly(codigo_wpp_FOB,mail_final,8,True,3)
+        division_mensajes["enviar"].append(mail)
     elif seleccion.upper() == "N":
-        continue
+        division_mensajes["no_enviar"].append(mail)
     else:
         # en realidad no se que deberia hacer en esta situacion, un continue??
         break
+
+print (division_mensajes)
+
+for mail in division_mensajes["enviar"]:
+
+    pywhatkit.sendwhatmsg_to_group_instantly(codigo_wpp_FOB,mail,8,True,3)
